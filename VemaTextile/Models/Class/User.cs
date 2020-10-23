58ebdda77418c4a1bb07f5a7d48da3fa8b5983be
Merve Mehmedoğlu <64040142+mervemehmedoglu@users.noreply.github.com>
2020-10-23 15:15:41 +0300

    User sınıfı oluşturuldu.

--- VemaTextile/Models/Class/User.cs
+++ VemaTextile/Models/Class/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,24 @@ namespace VemaTextile.Models.Class
 {
     public class User
     {
+        [Key]
         public int UserID { get; set; }
+        
+        [Column(TypeName = "nvarchar")]
+        [StringLength(30)]
+        public string UserAppellation { get; set; }
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string UserPassword { get; set; }
+        public DateTime UserAddedDate { get; set; }
+        public DateTime UserUpdatedDate { get; set; }
+        [Column(TypeName = "nvarchar")]
+        [StringLength(30)]
+        public string UserAddingStaff { get; set; }
+        [Column(TypeName = "nvarchar")]
+        [StringLength(30)]
+        public string UserUpdatingStaff { get; set; }
+
+            
     }
 }
