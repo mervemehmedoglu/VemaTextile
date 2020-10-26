760e2f42eb9f193e600aa52a2b868cc1b6453bd0
Merve Mehmedoğlu <64040142+mervemehmedoglu@users.noreply.github.com>
2020-10-26 16:56:40 +0300

    Alinan senetler ve fatura bilgileri eklendi.

--- VemaTextile/Models/Class/CRMCode.cs
+++ VemaTextile/Models/Class/CRMCode.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,33 @@ namespace VemaTextile.Models.Class
 {
     public class CRMCode
     {
+        [Key]
+        [Required]
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal Id { set; get; }
+
+        [Key]
+        [Required]
+        [Column(TypeName = "decimal(10,0)")]
+        public decimal KategoriId { set; get; }
+  
+        [Column(TypeName = "nvarchar")]
+        [StringLength(40)]
+        public string Aciklama{ set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(400)]
+        public string Aciklama2 { set; get; }
+
+        [Column(TypeName = "smallint")]
+        public int KodTipi { set; get; }
+
+        [Column(TypeName = "decimal(10,0)")]
+        public decimal SiraNo { set; get; }
+
+        [Key]
+        [Required]
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal Row_ID { set; get; }
     }
 }
--- VemaTextile/Models/Class/FaturaBilgileri.cs
+++ VemaTextile/Models/Class/FaturaBilgileri.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,94 @@ namespace VemaTextile.Models.Class
 {
     public class FaturaBilgileri
     {
+        [Required]
+        [Column(TypeName = "nvarchar")]
+        [StringLength(57)]
+        public string EvrakNO { set; get; }
+
+        public DateTime Tarih { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        public string CHKKodu { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        public string CHKAdi { set; get; }
+
+        [Required]
+        [Column(TypeName = "nvarchar")]
+        [StringLength(30)]
+        public string Tıp { set; get; }
+
+        [Column(TypeName = "money")]
+        public decimal Tutar { set; get; }
+
+        [Column(TypeName = "money")]
+        public decimal Oran { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(10)]
+        public string DovizTL{ set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string DovizCinsi { set; get; }
+
+        [Column(TypeName = "money")]
+        public decimal DovizKuru { set; get; }
+
+        [Column(TypeName = "money")]
+        public decimal DovizTutar { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string Kod1 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string Kod2 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string Kod3 { set; get; }
+
+        [Column(TypeName = "money")]
+        public string Kod4 { set; get; }
+
+        [Column(TypeName = "money")]
+        public string Kod5 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string TeslimAdres1 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string TeslimAdres2 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string TeslimAdres3 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string Aciklama1 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string Aciklama2 { set; get; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(10)]
+        public string KDVHesaplama { set; get; }
+
+        [Required]
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string KaynakEvrakTipi { set; get; }
+
+        [Key]
+        [Required]
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal Row_ID { set; get; }
     }
 }
