c7b962e691d3ec3d9bfac9d87201e3fc0f8bcc50
Merve Mehmedoğlu <64040142+mervemehmedoglu@users.noreply.github.com>
2020-11-02 14:31:34 +0300

    silindi.

--- VemaTextile/Models/Class/TaahhutAciklama.cs
+++ VemaTextile/Models/Class/TaahhutAciklama.cs
@@ -7,5 +7,14 @@ namespace VemaTextile.Models.Class
 {
     public class TaahhutAciklama
     {
+        [Column(TypeName = "nvarchar")]
+        [StringLength(8)]
+        public string EvrakNo { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(8)]
+        public string Seri { get; set; }
+
+
     }
 }
