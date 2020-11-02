c74a7bbb305ce7dca1b43e75162cce3d42039f81
Merve Mehmedoğlu <64040142+mervemehmedoglu@users.noreply.github.com>
2020-11-02 16:55:34 +0300

    düzenlendi.

--- VemaTextile/Models/Class/TaahhutPrimTakibi.cs
+++ VemaTextile/Models/Class/TaahhutPrimTakibi.cs
@@ -31,8 +31,43 @@ namespace VemaTextile.Models.Class
         public string FaturaNo { get; set; }
 
         [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string HesapKodu { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(50)]
+        public string Unvan { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string CihazMarka { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string CihazTipi { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string CihazModel { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(20)]
+        public string CihazSeriNo { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal CihazTutar { get; set; }
+
+        [Column(TypeName = "nvarchar")]
         [StringLength(10)]
-        public string FaturaTarihi { get; set; }
+        public string CalistirilmaTarihi { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [StringLength(8)]
+        public string PrimAlinmaDurumu { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,0)")]
+        public decimal Row_ID { get; set; }
 
 
 
